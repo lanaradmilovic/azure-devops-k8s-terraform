7d242d831256696f6a8adc5dc1e07e1a381590b4
Lana Radmilovic <lr20190131@student.fon.bg.ac.rs>
2024-08-22 01:41:02 +0200

    feat: added terraform configuration script for kubernetes cluster provisioning on azure

---
+++ terraform/configuration/iaac/azure/kubernetes/varibales.tf
@@ -0,0 +1,31 @@
+variable client_id {}
+variable client_secret {}
+variable ssh_public_key {}
+
+variable environment {
+  default = "dev"
+}
+
+variable location {
+  default = "westeurope"
+}
+
+variable node_count {
+  default = 3
+}
+
+variable dns_prefix {
+  default = "k8s"
+}
+
+variable cluster_name {
+  default = "k8s"
+}
+
+variable resource_group {
+  default = "kubernetes"
+}
+
+variable "vm_size" {
+  default = "standard_b2ms"
+}
